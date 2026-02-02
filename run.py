c69bd6c8084a21b7ef7bf5334b95a20e1d2b00c2
MichaelA <michaelalmosov@gmail.com>
2026-02-02 11:45:46 +0200

    Add Flask app factory under main package

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
-from main import create_app
+from .app import create_app
+
 
 # Call the Application Factory function to construct a Flask application instance
 # using the standard configuration defined in /instance/flask.cfg
 ## Run as python run.py
 
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent))
-
 from main import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
