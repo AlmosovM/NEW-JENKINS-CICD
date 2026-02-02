d62daaae2243e423b91d6b8a4397365d179a2035
MichaelA <michaelalmosov@gmail.com>
2026-02-02 11:48:09 +0200

    Fix run.py imports and expose create_app correctly

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-from .app import create_app
-
-
-# Call the Application Factory function to construct a Flask application instance
-# using the standard configuration defined in /instance/flask.cfg
-## Run as python run.py
-
 from main import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
